bcd0a3eade2c5c91e391dac811db7bb9b7eee98a
tedred <thetedred@gmail.com>
2025-01-09 21:00:37 -0800

    git all changes

--- src/All-routes/All-routes.js
+++ src/All-routes/All-routes.js
@@ -1,5 +1,4 @@
 const navigationBar = {
-<<<<<<< HEAD
     items: [
       { name: "Home", link: "/" },
       { name: "All Cars", link: "/all-cars" },
@@ -18,27 +17,3 @@ const navigationBar = {
   
   export default navigationBar;
   
-=======
-  items: [
-    { name: "Home", link: "/" },
-    { name: "All Cars", link: "/all-cars" },
-    { name: "Brands", link: "/brands" },
-    { name: "Offers", link: "/offers" },
-    { name: "Companies", link: "/companies" },
-    { name: "Contact Us", link: "/contact-us" },
-    { name: "About Us", link: "/about-us" },
-  ],
-};
-
-const footerLinks = {
-  items: [
-    { name: "Support", link: "/support" },
-    { name: "Terms and Conditions", link: "/terms-and-conditions" },
-    { name: "Privacy Policy", link: "/privacy-policy" },
-    { name: "Return Policy", link: "/return-policy" },
-    { name: "Carriers Page", link: "/carriers-page" },
-  ],
-};
-
-export { navigationBar, footerLinks };
->>>>>>> a659c508498994a848ebd61c944a1cee07b2061e
