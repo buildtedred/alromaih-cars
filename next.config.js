c9ae981805117c83f2602fe435f7ee9fb80ea911
tedred <thetedred@gmail.com>
2025-01-23 02:04:11 -0800

    m

--- next.config.js
+++ next.config.js
@@ -14,21 +14,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-<<<<<<< HEAD
         hostname: 'iili.io',
-=======
-        hostname: 'images.unsplash.com',
-        pathname: '/photo-**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'iili.io', // Add this line for iili.io
-        pathname: '/**', // Match all paths under iili.io
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
->>>>>>> 953d5cd913c4e2c9ab0c3c2a205de1f8bcfca7c0
         pathname: '/**',
       },
     ],
