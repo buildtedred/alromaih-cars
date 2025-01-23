64ae13323fa35fb23407986b4b5f16023fd170c0
tedred <thetedred@gmail.com>
2025-01-22 21:56:11 -0800

    add yes

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,6 @@ import { BrandsProvider } from "@/contexts/AllDataProvider";
 import { SlidesProvider } from "@/contexts/SliderContext";
 import CarCard from "@/MyComponents/CarCard/CarCar";
 import { HeroSection } from "@/MyComponents/HeroSection/HeroSection";
-import BrandShowcase from './../MyComponents/brandshowcase';
-import { FinancePartners } from './../MyComponents/FinancePartners';
-
 
 export default function Home() {
   return (
@@ -17,6 +14,7 @@ export default function Home() {
         <main className="min-h-screen p-8 bg-gray-100">
         <BrandsProvider>
             <CarCard />
+        </BrandsProvider>
           
         </main>
       </main>
