a3c300eacc9a35d46f2f223747b42d9ded2b69c4
tedred <thetedred@gmail.com>
2025-05-10 09:59:59 +0500

    s

--- src/app/api/supabasPrisma/carbrands/[id]/route.js
+++ src/app/api/supabasPrisma/carbrands/[id]/route.js
@@ -3,7 +3,7 @@ import prisma from "@/lib/prisma";
 
 export async function GET(request, { params }) {
   try {
-    const { id } = params;
+    const { id } = await params;
 
     const brand = await prisma.carBrand.findUnique({
       where: { id },
